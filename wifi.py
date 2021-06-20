ece33b5e1438f2e8c8d2f7bdc557e70f165f9281
hwj666 <he_wen_jie@126.com>
2021-06-20 21:44:16 +0800

    wifi.py

---
+++ wifi.py
@@ -0,0 +1,17 @@
+import network
+import time
+
+ssid='801'
+passwd='123456789.'
+def wifiConnect(ssid, password):
+    wifi = network.WLAN(network.STA_IF)
+    if not wifi.isconnected() :
+        print("will connect to wifi Route")
+        wifi.active(True)
+        wifi.connect(ssid, password)
+        while not wifi.isconnected():
+            time.sleep(1)
+            
+wifiConnect(ssid,passwd)
+
+
